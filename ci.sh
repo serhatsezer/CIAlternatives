fc55fcd5060a5a07ec3bc278dd670de91f051d47
Serhat Sezer <serhat.szr@gmail.com>
2019-11-17 23:10:20 +0200

    achanged shell script

--- ci.sh
+++ ci.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 8' test
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination generic/platform=iOS -configuration Release build CODE_SIGNING_ALLOWED=NO
 
-set -e
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator, name=iPhone 8' test
-xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
